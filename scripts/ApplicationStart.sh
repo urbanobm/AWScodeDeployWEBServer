feda0db49fabaa64f9ba463d5f1c6c261ee00a4c
Urbano Botrel Menegato <urbanobm@gmail.com>
2021-02-26 15:43:48 -0300

    Criação do projeto

---
+++ scripts/ApplicationStart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# start http service
+
+#sudo service httpd start
+sudo systemctl start httpd.service
---
+++ scripts/ApplicationStop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# stop http service
+
+#sudo service httpd stop
+sudo systemctl stop httpd.service
